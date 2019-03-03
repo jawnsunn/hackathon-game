15c52428499080d805584c0c1fe8e1d4620ffb30
jawnsunn <jawnsunn@gmail.com>
2019-03-02 23:21:13 -0800

    Creating animation states and attack control for player1
    Current problems: using getKeyDown -> getKeyUp is too slow for what we want to do. Not sure how to fix at the moment.

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    //Initialize and create classes here!
+    public Animator animator;
     Rigidbody2D rb2d;
+    bool isAttacking = false; //you're initialized in idle state, so you shouldn't be attacking
+    bool isHurt = false; //call this when making contacting with hitbox
+
     [SerializeField]
     float speed = 1.0f;
     //Vector2 velocity;
@@ -18,6 +23,8 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        animator.SetFloat("speed", Mathf.Abs(rb2d.velocity.x));
+        animator.SetBool("isAttacking", isAttacking);
         if (Input.GetKey("a"))
         {
             rb2d.velocity = new Vector2(-speed, 0f);
@@ -27,5 +34,22 @@ public class playerController : MonoBehaviour
         {
             rb2d.velocity = new Vector2(speed, 0f);
         }
+
+        if (Input.GetKey("d"))
+        {
+            isAttacking = true;
+        }
+
+        if (Input.GetKeyUp("d"))
+        {
+            isAttacking = false;
+        }
+
+        /*psuedocode:
+         * if (collide with otherPlayer.hitbox)
+         * {
+         *      isHurt = true;
+         *      end the match and use gameController to call gameOver state
+         * } */
     }
 }
